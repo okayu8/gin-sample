d0d82d05544824bb95180bb77209c6726d31878e
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-03-10 03:17:22 +0900

    first commit

---
+++ sample.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type C struct {
+	Id   int
+	Name string
+}
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.tmpl")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"a": "a",
+			"b": []string{"b_todo1", "b_todo2"},
+			"c": []C{{1, "c_mika"}, {2, "c_risa"}},
+			"d": C{3, "d_mayu"},
+			"e": true,
+			"f": false,
+			"h": true,
+		})
+	})
+	router.Run(":8082")
+}
